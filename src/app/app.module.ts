daa7c37fd5fe9a960c1e5f9fa5254bb644118df3
joel Dumortier <joel.dumortier@gmail.com>
2024-11-15 10:57:51 +0100

    create loader component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { LineChartComponent } from './charts/line-chart/line-chart.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoaderComponent } from './shared/loader/loader.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent, PieChartComponent, DetailsComponent, LineChartComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, PieChartComponent, DetailsComponent, LineChartComponent, LoaderComponent],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, HttpClientModule, NgxChartsModule],
   providers: [],
   bootstrap: [AppComponent],
