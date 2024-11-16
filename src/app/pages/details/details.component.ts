ee7ea978998d203561ce9187b449068e89b52fe9
joel Dumortier <joel.dumortier@gmail.com>
2024-11-16 22:18:41 +0100

    refactor code to initialize data if loaded and country participated otherwise redirected

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { map, Observable, take, tap } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { delay, map, Observable, Subject, take, takeUntil, tap } from 'rxjs';
 import { LineChartData } from 'src/app/core/models/LineChartData';
 import { OlympicCountry } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
@@ -8,40 +8,69 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrl: './details.component.scss'
+  styleUrl: './details.component.scss',
 })
-export class DetailsComponent implements OnInit{
-
-  public countryName: string ="";
+export class DetailsComponent implements OnInit, OnDestroy {
+  public countryName: string = '';
   public olympicCountry$!: Observable<OlympicCountry | undefined>;
-  public totalmedals$!: Observable<number>
-  public totalAthletes$!: Observable<number>
-  public totalEntries$!: Observable<number>
-  public medalsByParticipation$!: Observable<LineChartData[]>
-  public error$!: Observable<string | null>;
-  public isLoading$! : Observable<boolean>;
+  public totalmedals$!: Observable<number>;
+  public totalAthletes$!: Observable<number>;
+  public totalEntries$!: Observable<number>;
+  public medalsByParticipation$!: Observable<LineChartData[]>;
+  public isLoading: boolean = true;
+  private destroy$ = new Subject<boolean>();
 
-  constructor(private route : ActivatedRoute, private olympicService : OlympicService){}
-
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private olympicService: OlympicService
+  ) {}
+  
   ngOnInit(): void {
-    this.getCounrtyNameFromRoute();
-    this.initData();
+    this.getCountryNameFromRoute();
+    this.initDataOrRedirect();
   }
 
-  private getCounrtyNameFromRoute(){
-    this.route.params
-    .pipe(
-      take(1)
+  private getCountryNameFromRoute() {
+    this.countryName = this.route.snapshot.params['countryName'];
+  }
+
+  private initDataOrRedirect() {
+    this.olympicService.getLoadindStatus()
+    .pipe(takeUntil(this.destroy$))
+    .subscribe(
+      (isloading) => {
+        if (!isloading) {
+          this.isLoading = false;
+          this.initDataIfParticipated();
+        }
+      }
     )
-    .subscribe(params => this.countryName = params['countryName'])
   }
 
-  private initData(){
+  private initDataIfParticipated() {
+    this.olympicService.getOlympicCountryByName(this.countryName)
+    .pipe(takeUntil(this.destroy$))
+    .subscribe(
+      (response) => {
+        if (response) {
+          this.initData()
+        } else (
+          this.router.navigate(["/not-found"])
+        )
+      }
+    )
+  }
+  
+
+  private initData() {
     this.totalmedals$ = this.olympicService.getMedals(this.countryName);
     this.totalAthletes$ = this.olympicService.getAthletes(this.countryName);
-    this.totalEntries$ = this.olympicService.getParticipations(this.countryName).pipe(map(participations => participations.length));
+    this.totalEntries$ = this.olympicService.getParticipations(this.countryName).pipe(map((participations) => participations.length));
     this.medalsByParticipation$ = this.olympicService.getmedalsByParticipation(this.countryName);
-    this.error$ = this.olympicService.getError();
-    this.isLoading$ = this.olympicService.getLoadindStatus();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
   }
 }
